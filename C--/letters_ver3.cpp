6c2bfe22354f56816e3786f947bd0d3d4b2247af
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2023-11-04 18:47:47 +0700

    Add files via upload

---
+++ C--/letters_ver3.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nothing ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pb push_back
+#define endl "\n"
+const ll oo = 0x3f3f3f3f;
+const ll mod = 1e9 + 7;
+const int maxn = 1e6 + 5;
+const int maxa = 2e3 + 5;
+
+ll n, m, a[maxn + 2], ite[maxn + 2], sum[maxn + 2];
+
+int main() {
+    nothing
+    if (fopen("letters.inp", "r")) {
+        freopen("letters.inp", "r", stdin);
+        freopen("letters.out", "w", stdout);
+    }
+    cin >> n >> m;
+    
+    sum[0] = 0;
+    for (int i = 1; i <= n; ++i) {
+    	cin >> a[i];
+    	sum[i] = sum[i - 1] + a[i];
+    }
+    for (int i = 1; i <= m; ++i) {
+    	cin >> ite[i];
+    }
+    ite[m + 1] = +oo;
+    
+    int j = 1;
+    for (int i = 1; i <= n; ++i) {
+    	while (ite[j] <= sum[i]) {
+    		cout << i << " " << ite[j] - sum[i - 1] << endl; 
+    		++j;
+    	}
+    }
+    
+    
+	return 0;
+}
