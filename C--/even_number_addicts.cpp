f748c4d8cceff51b80c19e1757c59735933f3748
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2024-03-14 23:16:41 +0700

    Add files via upload

---
+++ C--/even_number_addicts.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nothing ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pb push_back
+const ll oo = 1e13;
+const ll modd = 1e9 + 7;
+const int maxn = 1e6 + 5;
+const int maxa = 2e3 + 5;
+
+ll t, n, a[maxn + 2];
+ll even = 0, odd = 0;
+
+int main() {
+    nothing
+    if (fopen("ss.inp", "r")) {
+        freopen("ss.inp", "r", stdin);
+        freopen("ss.out", "w", stdout);
+    }
+    cin >> t;
+    
+    while (t--) {
+    	cin >> n;
+    	
+    	even = 0; odd = 0;
+    	for (int i = 1; i <= n; ++i) {
+    		cin >> a[i];
+    		
+    		if (a[i] % 2 == 0) {
+    			even++;
+    		}
+    		else {
+    			odd++;
+    		}
+    	}
+    	
+    	if (odd % 4 == 2) {
+    		cout << "Bob" << endl;
+    	}
+    	else if (odd % 4 == 3) {
+    		cout << "Alice" << endl;
+    	}
+    	else if (odd % 4 == 0) {
+    		cout << "Alice" << endl;
+    	}
+    	else if (even % 2 == 1) {
+    		cout << "Alice" << endl;
+    	}
+    	else {
+    		cout << "Bob" << endl;
+    	}
+    }
+    return 0;
+}
+
+// https://codeforces.com/problemset/problem/1738/C
