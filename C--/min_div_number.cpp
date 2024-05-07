afd159ccb5bb5a5d8741b070691901f4db3d048f
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2024-05-07 23:18:41 +0700

    Add files via upload

---
+++ C--/min_div_number.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nothing ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pb push_back
+const ll oo = 1e13;
+const ll modd = 1e9 + 7;
+const int maxn = 1e6 + 5;
+const int maxa = 2e3 + 5;
+
+ll x, k;
+int main() {
+    nothing
+    if (fopen("ss.inp", "r")) {
+        freopen("ss.inp", "r", stdin);
+        freopen("ss.out", "w", stdout);
+    }
+    cin >> x >> k;
+    
+    cout << k / __gcd(x, k);
+    return 0;
+}
+
+// https://oj.vnoi.info/problem/bedao_r19_a
