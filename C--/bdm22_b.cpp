bfe6c61fe0508113391083cba65929fdd6350fb0
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2024-05-27 21:54:47 +0700

    Add files via upload

---
+++ C--/bdm22_b.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nothing ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pb push_back
+const ll oo = 1e13;
+const ll modd = 1e9 + 7;
+const int maxn = 1e6 + 5;
+const int maxa = 2e3 + 5;
+const int base = 31;
+
+ll n, a[maxn + 2], ans = 0;
+map<ll, ll> cnt;
+
+int main() {
+    nothing
+    if (fopen("ss.inp", "r")) {
+        freopen("ss.inp", "r", stdin);
+        freopen("ss.out", "w", stdout);
+    }
+    cin >> n;
+    
+    for (int i = 1; i <= n; ++i) {
+    	cin >> a[i];
+    	if (i >= 7) cnt[a[i - 6] % 23]++;
+    	
+    	ans += cnt[a[i] % 23];
+    }
+    
+    cout << ans << endl;
+    return 0;
+}
+
+// https://oj.vnoi.info/problem/bedao_m22_b
