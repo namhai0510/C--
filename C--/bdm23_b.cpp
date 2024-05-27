45120a7cc368b7303f05bfa23711894d478ec352
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2024-05-27 14:57:55 +0700

    Add files via upload

---
+++ C--/bdm23_b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nothing ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pb push_back
+const ll oo = 1e13;
+const ll modd = 1e9 + 7;
+const int maxn = 1e6 + 5;
+const int maxa = 2e3 + 5;
+const int base = 31;
+
+ll test, n, m;
+
+int main() {
+    nothing
+    if (fopen("ss.inp", "r")) {
+        freopen("ss.inp", "r", stdin);
+        freopen("ss.out", "w", stdout);
+    }
+    cin >> test;
+    
+    while (test--) {
+    	cin >> n >> m;
+    	
+    	if (n % (m + 1) != 0) cout << "A" << endl;
+    	else cout << "B" << endl;
+    }
+    return 0;
+}
+
+// https://oj.vnoi.info/problem/bedao_m23_b
