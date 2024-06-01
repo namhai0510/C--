c9ba0b86eee197d17d9e0fbd2aedb8a8dafcc5a7
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2024-06-01 17:18:59 +0700

    Add files via upload

--- C--/purple02C.cpp
+++ C--/purple02C.cpp
@@ -14,8 +14,33 @@ const ll maxn = 1e6 + 2;
 const ll maxa = 2e3 + 2;
 
 string p[] = {"0", "ABC", "ACB", "BAC", "BCA", "CAB", "CBA"};
-ll test, n, cnt[7];
+ll test, n;
 string s;
+
+bool Solve(ll x1, ll x2, ll x3, ll x4, ll x5, ll x6) {
+	int now = 1, it = 0;
+	
+	for (int i = 1; i <= n; ++i) {
+		if (now == 1) it = x1;
+		if (now == 2) it = x2;
+		if (now == 3) it = x3;
+		if (now == 4) it = x4;
+		if (now == 5) it = x5;
+		if (now == 6) it = x6;
+		
+		if (now >= 7) return 1;
+		
+		if (s[i] == p[it][0]) {
+			if (s[i + 1] == p[it][1] && s[i + 2] == p[it][2]) now++, i += 2;
+		}
+	}
+	
+	// cout << now << endl;
+	if (now >= 7) return 1;
+	else return 0;
+}
+
+bool ok = 0;
 
 int main() {
 	nothing
@@ -30,38 +55,61 @@ int main() {
 		
 		n = s.size();
 		s = '#' + s + "##";
-		
-		for (int i = 1; i <= 6; ++i) cnt[i] = 0;
+		ok = 0;
 		
 		if (s.size() < 19) {
 			cout << "NO" << endl;
 			continue;
 		}
 		
-		for (int i = 1; i <= n; ++i) {
-			if (s[i] == 'A') {
-				if (s[i + 1] == 'B' && s[i + 2] == 'C') cnt[1]++, i += 2;
-				else if (s[i + 1] == 'C' && s[i + 2] == 'B') cnt[2]++, i += 2;
-			}
-			else if (s[i] == 'B') {
-				if (s[i + 1] == 'A' && s[i + 2] == 'C') cnt[3]++, i += 2;
-				else if (s[i + 1] == 'C' && s[i + 2] == 'A') cnt[4]++, i += 2;
-			}
-			else if (s[i] == 'C') {
-				if (s[i + 1] == 'A' && s[i + 2] == 'B') cnt[5]++, i += 2;
-				else if (s[i + 1] == 'B' && s[i + 2] == 'A') cnt[6]++, i += 2;
+		for (int p1 = 1; p1 <= 6; ++p1) {
+			for (int p2 = 1; p2 <= 6; ++p2) {
+				if (ok) break;
+				
+				if (p2 == p1) continue;
+				
+				for (int p3 = 1; p3 <= 6; ++p3) {
+					if (ok) break;
+					
+					if (p3 == p1) continue;
+					if (p3 == p2) continue;
+					
+					for (int p4 = 1; p4 <= 6; ++p4) {
+						if (ok) break;
+						
+						if (p4 == p1) continue;
+						if (p4 == p2) continue;
+						if (p4 == p3) continue;
+						
+						for (int p5 = 1; p5 <= 6; ++p5) {
+							if (ok) break;
+							
+							if (p5 == p1) continue;
+							if (p5 == p2) continue;
+							if (p5 == p3) continue;
+							if (p5 == p4) continue;
+							
+							for (int p6 = 1; p6 <= 6; ++p6) {
+								if (ok) break;
+								
+								if (p6 == p1) continue;
+								if (p6 == p2) continue;
+								if (p6 == p3) continue;
+								if (p6 == p4) continue;
+								if (p6 == p5) continue;
+								
+								if (Solve(p1, p2, p3, p4, p5, p6)) {
+									ok = 1;
+									break;
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 		
-		bool check = 1;
-		for (int i = 1; i <= 6; ++i) {
-			if (cnt[i] == 0)  {
-				check = false;
-			 	break;
-			}
-		}
-			
-		if (check) {
+		if (ok) {
 			cout << "YES" << endl;
 		}
 		else cout << "NO" << endl;
