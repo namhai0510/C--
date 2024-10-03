ace9a44b656e7664ac1b11bcb210fec410f1aa6c
namhai0510 <117525433+namhai0510@users.noreply.github.com>
2024-10-03 21:40:40 +0700

    Add files via upload

---
+++ C--/bdm25_a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define nothing ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl "\n"
+#define ll long long
+#define ull unsigned long long
+#define fi first
+#define se second
+#define pb push_back
+const ll oo = 1e13;
+const ll mod = 1e9 + 7;
+const int maxn = 1e6;
+const int maxa = 2e3;
+const int base = 31;
+
+ll n, ans = 0;
+vector<ll> a[3];
+
+int main() {
+    nothing
+    if (fopen("ss.inp", "r")) {
+        freopen("ss.inp", "r", stdin);
+        freopen("ss.out", "w", stdout);
+    }
+    
+    cin >> n;
+    
+    for (int i = 1; i <= n; ++i) {
+    	ll x; cin >> x;
+    	
+    	a[x % 2].pb(x);
+    }
+    
+    for (int k = 0; k <= 1; ++k)
+	    for (int i = 0; i < a[k].size(); ++i)  {
+	    	ans += a[k][i] * a[!k].size();
+	    	ans %= mod;
+	    }
+	    	
+	cout << ans % mod;
+    	
+    
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n"; 
+    return 0;
+}
